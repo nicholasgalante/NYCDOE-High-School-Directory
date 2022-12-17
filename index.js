7534a0f2bcc82721c9889a497605d3aab53102e9
Nicholas <nicholasgalante94@gmail.com>
2022-12-17 15:14:44 -0500

    Add live search function

--- index.js
+++ index.js
@@ -123,3 +123,15 @@ createCard = (data) => {
 
 }
 
+liveSearch = () => {
+    let cards = document.querySelectorAll('details');
+    let search_query = document.getElementById("search").value;
+    for (var i = 0; i < cards.length; i++) {
+        if(cards[i].innerText.toLowerCase()
+          .includes(search_query.toLowerCase())) {
+            cards[i].classList.remove("is-hidden");
+        } else {
+          cards[i].classList.add("is-hidden");
+        }
+    }
+}
